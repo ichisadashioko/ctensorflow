730dd98b8082f9c9665b43155464cb814358af18
shioko <ichisadashioko@gmail.com>
2021-01-16 11:39:55 +0000

    :rocket: load savedmodel with C API

--- ctensorflow/src/main.c
+++ ctensorflow/src/main.c
@@ -1,48 +1,21 @@
 #define _CRT_SECURE_NO_DEPRECATE
+#include <direct.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <direct.h>
+#include <memory.h>
 
 #include <tensorflow/c/c_api.h>
 #pragma comment(lib, "tensorflow.lib")
 
-void free_buffer(void* data, size_t length) {
-	free(data);
-}
+#define SAVED_MODEL_NUM_TAGS 1
 
-TF_Buffer* read_file(const char* fpath) {
-	FILE* fp = fopen(fpath, "rb");
+void doNothingDeallocator(void* data, size_t len, void* arg) {}
 
-	if (fp == NULL) {
-		printf("Failed to open file %s\n", fpath);
-		printf("%s:%d\n", __FILE__, __LINE__);
-		exit(1);
+int main(int argc, char* argv[]) {
+	for (int i = 0; i < argc; i++) {
+		printf("%d - %s\n", i, argv[i]);
 	}
 
-	fseek(fp, 0, SEEK_END);
-	long fsize = ftell(fp);
-	fseek(fp, 0, SEEK_SET); // same as rewind(f)
-
-	void* data = malloc(fsize);
-
-	if (data == NULL) {
-		printf("Failed to call malloc!\n");
-		printf("%s:%d\n", __FILE__, __LINE__);
-		exit(1);
-	}
-
-	fread(data, fsize, 1, fp);
-	fclose(fp);
-
-	TF_Buffer* buf = TF_NewBuffer();
-	buf->data = data;
-	buf->length = fsize;
-	buf->data_deallocator = free_buffer;
-	return buf;
-}
-
-int main()
-{
 	char* buffer;
 
 	// Get the current working directory
@@ -54,32 +27,182 @@ int main()
 		free(buffer);
 	}
 
-
 	printf("Hello from TensorFlow C library version %s\n", TF_Version());
 
-	TF_Buffer* graph_def = read_file("tensorflow_inception_graph.pb");
+	// look at the output of cwd above and move the SavedModel directory to where is appropriate while debuging with Visual Studio / your IDE
+	// TODO: use argv[0] to resolve the SavedModel path
+	const char* SAVED_MODEL_DIR = "etl9b.model.tensorflow.savedmodel/";
+	const char* SAVED_MODEL_TAG = "serve";
+
 	TF_Graph* graph = TF_NewGraph();
+	TF_Status* tfStatus = TF_NewStatus();
+	TF_SessionOptions* tfSessOptions = TF_NewSessionOptions();
+	TF_Buffer* tfRunOptions = NULL;
 
-	// Import graph_def into graph
-	TF_Status* status = TF_NewStatus();
-	TF_ImportGraphDefOptions* opts = TF_NewImportGraphDefOptions();
-	TF_GraphImportGraphDef(graph, graph_def, opts, status);
-	TF_DeleteImportGraphDefOptions(opts);
+	TF_Session* tfSession = TF_LoadSessionFromSavedModel(
+		tfSessOptions, tfRunOptions, SAVED_MODEL_DIR, &SAVED_MODEL_TAG,
+		SAVED_MODEL_NUM_TAGS, graph, NULL, tfStatus);
 
-	if (TF_GetCode(status) != TF_OK) {
-		//fprintf(stderr, "ERROR: Unable to import graph %s\n", TF_Message(status));
-		printf("ERROR: Unable to import graph %s\n", TF_Message(status));
-		return 1;
+	if (TF_GetCode(tfStatus) == TF_OK) {
+		printf("TF_LoadSessionFromSavedModel OK\n");
+
+		// get input tensor
+		int numInputs = 1;
+		TF_Output* inputTensor = malloc(sizeof(TF_Output) * numInputs);
+		if (inputTensor == NULL) {
+			printf("ERROR: %s:%d\n", __FILE__, __LINE__);
+			printf("malloc failed!\n");
+		}
+		else {
+			TF_Output t0 = { .oper = TF_GraphOperationByName(
+								graph, "serving_default_conv2d_00_input"),
+							.index = 0 };
+
+			if (t0.oper == NULL) {
+				printf("ERROR: %s:%d\n", __FILE__, __LINE__);
+				printf("TF_GraphOperationByName failed!\n");
+			}
+			else {
+				printf("TF_GraphOperationByName OK\n");
+
+				inputTensor[0] = t0;
+
+				// get output tensor
+				int numOutputs = 1;
+				TF_Output* outputTensor = malloc(sizeof(TF_Output) * numOutputs);
+
+				if (outputTensor == NULL) {
+					printf("ERROR: %s:%d\n", __FILE__, __LINE__);
+					printf("malloc failed!\n");
+				}
+				else {
+					TF_Output t2 = {
+						.oper = TF_GraphOperationByName(graph, "StatefulPartitionedCall"),
+						.index = 0 };
+
+					if (t2.oper == NULL) {
+						printf("ERROR: %s:%d\n", __FILE__, __LINE__);
+						printf("TF_GraphOperationByName failed!\n");
+					}
+					else {
+						printf("TF_GraphOperationByName OK\n");
+
+						outputTensor[0] = t2;
+
+						// allocate data for inputs and outputs
+						TF_Tensor** inputValues =
+							(TF_Tensor**)malloc(sizeof(TF_Tensor*) * numInputs);
+						if (inputValues == NULL) {
+							printf("ERROR: %s:%d\n", __FILE__, __LINE__);
+							printf("malloc failed!\n");
+						}
+						else {
+							TF_Tensor** outputValues =
+								(TF_Tensor**)malloc(sizeof(TF_Tensor*) * numOutputs);
+							if (outputValues == NULL) {
+								printf("ERROR: %s:%d\n", __FILE__, __LINE__);
+								printf("malloc failed!\n");
+							}
+							else {
+								int ndimsOfInputs = 4; // (-1, 64, 64, 1)
+								int64_t inputDims[] = { 1, 64, 64, 1 };
+
+								// sizeof("pixel_data_type") * num_input_images * height * width * num_channels
+								size_t inputDataByteCount = sizeof(float) * 1 * 64 * 64 * 1;
+								float* sampleInputData = malloc(inputDataByteCount);
+
+								if (sampleInputData == NULL) {
+									printf("ERROR: %s:%d\n", __FILE__, __LINE__);
+									printf("malloc failed!\n");
+								}
+								else {
+									// zeroing out memory
+									memset(sampleInputData, 0, inputDataByteCount);
+
+									/*
+									* TF_NewTensor function signature
+									*
+									* TF_CAPI_EXPORT extern TF_Tensor* TF_NewTensor(
+									*	TF_DataType, const int64_t* dims, int num_dims, void* data, size_t len,
+									*	void (*deallocator)(void* data, size_t len, void* arg),
+									*	void* deallocator_arg);
+									*/
+									TF_Tensor* floatInputTensor = TF_NewTensor(
+										TF_FLOAT,              // TF_DataType
+										inputDims,             // const int64_t* dims
+										ndimsOfInputs,         // int num_dims
+										sampleInputData,       // void* data
+										inputDataByteCount,    // size_t len
+										&doNothingDeallocator, // void* arg
+										0                      // void* deallocator_arg
+									);
+
+									if (floatInputTensor == NULL) {
+										printf("ERROR: %s:%d\n", __FILE__, __LINE__);
+										printf("TF_NewTensor failed!\n");
+									}
+									else {
+										printf("TF_NewTensor OK\n");
+
+										inputValues[0] = floatInputTensor;
+
+										// run the session
+										TF_SessionRun(tfSession, NULL, inputTensor, inputValues,
+											numInputs, outputTensor, outputValues,
+											numOutputs, NULL, 0, NULL, tfStatus);
+
+										if (TF_GetCode(tfStatus) == TF_OK) {
+											printf("TF_SessionRun OK\n");
+
+											void* outputDataVoidPtr = TF_TensorData(outputValues[0]);
+
+											float* outputData = (float*)outputDataVoidPtr;
+											float maxOutputValue = outputData[0];
+											int maxOutputIdx = 0;
+
+											// my model's output will have shape of (1, 3001)
+											for (int i = 1; i < 3001; i++) {
+												if (outputData[i] > maxOutputValue) {
+													maxOutputValue = outputData[i];
+													maxOutputIdx = i;
+												}
+											}
+
+											printf("maxOutputIdx: %d\n", maxOutputIdx);
+										}
+										else {
+											printf("ERROR: %s:%d\n", __FILE__, __LINE__);
+											printf("TF_SessionRun failed\n");
+											printf("%s\n", TF_Message(tfStatus));
+										}
+									}
+
+									free(sampleInputData);
+								}
+
+								free(outputValues);
+							}
+
+							free(inputValues);
+						}
+					}
+
+					free(outputTensor);
+				}
+			}
+
+			free(inputTensor);
+		}
+	}
+	else {
+		printf("ERROR: %s:%d\n", __FILE__, __LINE__);
+		printf("%s\n", TF_Message(tfStatus));
 	}
 
-	//fprintf(stdout, "Successfully imported graph\n");
-	printf("Successfully imported graph\n");
-	TF_DeleteStatus(status);
-	TF_DeleteBuffer(graph_def);
-
-	// Use the graph
-
 	TF_DeleteGraph(graph);
-	return 0;
+	TF_DeleteSession(tfSession, tfStatus);
+	TF_DeleteSessionOptions(tfSessOptions);
+	TF_DeleteStatus(tfStatus);
+
 	return 0;
 }
--- download_deps.py
+++ download_deps.py
@@ -2,24 +2,24 @@ import os
 import zipfile
 from urllib.request import urlretrieve
 
-dest_dir = 'dependencies'
+dependencies_dl_dir = 'dependencies'
 
-if not os.path.exists(dest_dir):
-    os.makedirs(dest_dir)
+if not os.path.exists(dependencies_dl_dir):
+    os.makedirs(dependencies_dl_dir)
 
 # Download tensorflow library
 
-tf_lib_url = 'https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-windows-x86_64-2.3.1.zip'
+tf_lib_archive_url = 'https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-windows-x86_64-2.3.1.zip'
 
-tf_lib_dest_filename = tf_lib_url[tf_lib_url.rfind('/')+1:]
-tf_lib_dest_fpath = os.path.join(dest_dir, tf_lib_dest_filename)
+tf_lib_archive_filename = tf_lib_archive_url[tf_lib_archive_url.rfind('/')+1:]
+tf_lib_archive_dl_fpath = os.path.join(dependencies_dl_dir, tf_lib_archive_filename)
 
-if not os.path.exists(tf_lib_dest_fpath):
-    print('Downloading', tf_lib_dest_filename)
-    print(tf_lib_url)
-    urlretrieve(tf_lib_url, tf_lib_dest_fpath)
+if not os.path.exists(tf_lib_archive_dl_fpath):
+    print('Downloading', tf_lib_archive_filename)
+    print(tf_lib_archive_url)
+    urlretrieve(tf_lib_archive_url, tf_lib_archive_dl_fpath)
 
-zip_ref = zipfile.ZipFile(tf_lib_dest_fpath, 'r')
+zip_ref = zipfile.ZipFile(tf_lib_archive_dl_fpath, 'r')
 
 required_files = []
 
@@ -28,14 +28,14 @@ for zip_file_ref in zip_ref.filelist:
         required_files.append(zip_file_ref)
 
 for zip_file_ref in required_files:
-    extract_dest = os.path.join(dest_dir, zip_file_ref.filename)
+    extract_dest = os.path.join(dependencies_dl_dir, zip_file_ref.filename)
     if not os.path.exists(extract_dest):
         print('Inflating', extract_dest)
 
-        extract_dest_dir, _ = os.path.split(extract_dest)
+        extract_dependencies_dl_dir, _ = os.path.split(extract_dest)
 
-        if not os.path.exists(extract_dest_dir):
-            os.makedirs(extract_dest_dir)
+        if not os.path.exists(extract_dependencies_dl_dir):
+            os.makedirs(extract_dependencies_dl_dir)
 
         with open(extract_dest, mode='wb') as outfile:
             outfile.write(zip_ref.read(zip_file_ref.filename))
@@ -45,23 +45,23 @@ for zip_file_ref in required_files:
 
 zip_ref.close()
 
-# Download inception graph for demo
-inception_graph_archive_url = 'https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip'
-inception_graph_archive_filename = inception_graph_archive_url[inception_graph_archive_url.rfind('/')+1:]
-inception_graph_archive_fpath = os.path.join(dest_dir, inception_graph_archive_filename)
+# Download my own model for using load SavedModel API
 
-if not os.path.exists(inception_graph_archive_fpath):
-    print('Downloading', inception_graph_archive_filename)
-    print(inception_graph_archive_url)
-    urlretrieve(inception_graph_archive_url, inception_graph_archive_fpath)
+savedmodel_archive_url = 'https://github.com/ichisadashioko/kanji-recognition/releases/download/v0.2/etl9b.model.tensorflow.savedmodel.zip'
+savedmodel_archive_filename = savedmodel_archive_url[savedmodel_archive_url.rfind('/')+1:]
+savedmodel_archive_dl_fpath = os.path.join(dependencies_dl_dir, savedmodel_archive_filename)
 
-inception_graph_entry_name = 'tensorflow_inception_graph.pb'
-inception_graph_extracted_fpath = os.path.join(dest_dir, inception_graph_entry_name)
+if not os.path.exists(savedmodel_archive_dl_fpath):
+    print('Downloading', savedmodel_archive_filename)
+    print(savedmodel_archive_url)
+    urlretrieve(savedmodel_archive_url, savedmodel_archive_dl_fpath)
 
-if not os.path.exists(inception_graph_extracted_fpath):
-    zip_ref = zipfile.ZipFile(inception_graph_archive_fpath, 'r')
+savedmodel_dir_name = 'etl9b.model.tensorflow.savedmodel'
+savedmodel_dir_extracting_fpath = os.path.join(dependencies_dl_dir, savedmodel_dir_name)
 
-    with open(inception_graph_extracted_fpath, mode='wb') as outfile:
-        outfile.write(zip_ref.read(inception_graph_entry_name))
+if not os.path.exists(savedmodel_dir_extracting_fpath):
+    print('Inflating ' + savedmodel_dir_extracting_fpath)
 
+    zip_ref = zipfile.ZipFile(savedmodel_archive_dl_fpath, 'r')
+    zip_ref.extractall(path=dependencies_dl_dir)
     zip_ref.close()
